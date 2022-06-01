6d08699cc07286504743fb7fa8f07f561093e01f
SheeranJL <lewisjsheeran@protonmail.com>
2022-06-01 15:00:06 +1000

    refactored code

--- src/components/buttons/custom-button.js
+++ src/components/buttons/custom-button.js
@@ -6,12 +6,11 @@ import './custom-button.scss';
 const CustomButton = ({children, clickEvent, type, fill, handleSubmit, textColour}) => {
   return (
     <button
-      className='button'
+      className={type === 'message' ? 'button button-message' : 'button'}
       onClick={clickEvent}
       style={{
         backgroundColor: fill ? '#7510F7': '',
         color: textColour,
-        font: type === 'message' ? 'normal normal bold 19px/41px Open Sans' : null
         }}>
       {children}
     </button>
